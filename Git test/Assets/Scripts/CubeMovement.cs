fb8519a16bfebb4ffcfa0b48dbbe2a6d88a9e5e7
DF <d.ferenchuk@gmail.com>
2016-02-16 22:50:51 +0200

    some change

--- Git test/Assets/Scripts/CubeMovement.cs
+++ Git test/Assets/Scripts/CubeMovement.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CubeMovement : MonoBehaviour {
+public class CubeMovement : MonoBehaviour
+{
 
-	public float speed = 3.0f;
+    public float speed = 3.0f;
+    public Transform endPoint;
+     Vector3 startPoint;
+    public Dir direction = Dir.left;
+    // Use this for initialization
+    void Start()
+    {
+        startPoint = transform.position;
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		this.transform.position += Vector3.up * Time.deltaTime * this.speed;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == Dir.left)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, Time.deltaTime * speed);
+            if (transform.position == endPoint.position)
+                direction = Dir.right;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPoint, Time.deltaTime * speed);
+            if (transform.position == startPoint)
+                direction = Dir.left;
+        }
+    }
+
+    public enum Dir
+    {
+        right,
+        left
+    }
 }
